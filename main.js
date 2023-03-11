c686a5b3c76463753642396928a9f1d29b1c6586
ahmed elbqarey <amedo56734@gmail.com>
2023-03-11 18:04:49 +0200

    upload file

---
+++ main.js
@@ -0,0 +1,16 @@
+let num1 = 0;
+let num2 = 0;
+let num3 = 0;
+
+function plus1() {
+  ++num1;
+  document.getElementById("button1").innerHTML = num1;
+}
+function plus2() {
+  ++num2;
+  document.getElementById("button2").innerHTML = num2;
+}
+function plus3() {
+  ++num3;
+  document.getElementById("button3").innerHTML = num3;
+}
